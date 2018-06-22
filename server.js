6ac68ec808da0980a0b01e0c2ebf486bf3a6cc7d
John Erick Madduma <madduma.johnerick@gmail.com>
2018-06-22 22:55:03 +0800

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res) {
+  console.log("IM HERE")
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log(`Running on port ${port}....`);
